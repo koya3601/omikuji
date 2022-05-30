1857872d5403be4ac3971d2450300b6a1d34c347
高橋晃也 <kouuuuu10@gmail.com>
2022-05-30 11:54:00 +0900

    Initial Commit

---
+++ omikuji.js
@@ -0,0 +1,26 @@
+let user_name;
+let user_result;
+
+user_name = prompt("お名前を教えて下さい。");
+if (user_name == "") {
+  user_name = "名無し";
+}
+document.getElementById("name").innerHTML = user_name;
+
+let rand = Math.floor( Math.random() * 5);
+if (rand == 0) {
+  user_result = "大吉";
+}
+if (rand == 1) {
+  user_result = "中吉";
+}
+if (rand == 2) {
+  user_result = "小吉";
+}
+if (rand == 3) {
+  user_result = "吉";
+}
+if (rand == 4) {
+  user_result = "凶";
+}
+document.getElementById("result").innerHTML = user_result;
